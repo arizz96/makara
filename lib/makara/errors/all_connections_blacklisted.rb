41a1d9da8272f356e19953c5fb249924afc1ddf2
arizz96 <arizz96@gmail.com>
2020-07-01 23:20:11 +0200

    Include role in AllConnectionsBlacklisted error

--- lib/makara/errors/all_connections_blacklisted.rb
+++ lib/makara/errors/all_connections_blacklisted.rb
@@ -2,7 +2,10 @@ module Makara
   module Errors
     class AllConnectionsBlacklisted < MakaraError
 
+      attr_reader :role
+
       def initialize(pool, errors)
+        @role = pool.role
         errors = [*errors]
         messages = errors.empty? ? 'No error details' : errors.map(&:message).join(' -> ')
         super "[Makara/#{pool.role}] All connections are blacklisted -> " + messages
